744ae02dbc3d0c36ec8e043484cc4bb01d259531
Gabrielle Piccininni <piccingt@gmail.com>
2016-05-16 11:02:03 -0400

    update

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,11 @@
 
-	//GOAL: redirect to URL when user checks "I don't like pasta option"
+	//GOAL: Open lightbox when user checks "I don't like pasta option"
 	//1. user clicks 3rd radio button
 
 	// $('#noPasta').click(redirect);
+$(document).ready(function(){
 
-		$('#noPasta').ekkoLightbox(options);
+		$('#noPasta').click(redirect);
 
 	//2. Browser redirects
 	function redirect() {
@@ -12,4 +13,3 @@
 		window.location.href = 'https://media.giphy.com/media/l2R05FZf4dVOrgIaQ/giphy.gif';
 	};
 });
-
