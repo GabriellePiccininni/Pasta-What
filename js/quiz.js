7a724be819191c7ff8e348ddbe901f2d7dd29847
Gabrielle Piccininni <piccingt@gmail.com>
2016-09-24 14:32:22 -0400

    quiz js

--- js/quiz.js
+++ js/quiz.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 			'<div id="lightbox">' +
 				'<p>Click to close</p>' +
 				'<div id="content">' + //insert clicked link's href into img src
-					'<img src="https://cdn4.dogonews.com/images/4608011a-7a08-4720-9e40-a4e28b0ebedd/summer_time-1455175.jpg"/>' +
+					'<img src="http://media1.giphy.com/media/l2R05FZf4dVOrgIaQ/giphy.gif"/>' +
 				'</div>' +	
 			'</div>';
 				
@@ -26,4 +26,110 @@ $(document).ready(function(){
 	
 	});
 
+
+	$('#godfather').click(function(e){
+		e.preventDefault();
+		if ($('#godfather').val() === "a"){
+			console.log('happening')
+			var lightbox = 
+			'<div id="lightbox">' +
+				'<p>Click to close</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<iframe width="560" height="315" src="https://www.youtube.com/embed/ppjyB2MpxBU" frameborder="0" allowfullscreen></iframe>' +
+				'</div>' +	
+			'</div>';
+				
+			//insert lightbox HTML into page
+			$('body').prepend(lightbox);
+		}
+
+	//Click anywhere on the page to get rid of lightbox window
+	$('#lightbox').on('click', function() {
+		$('#lightbox').hide();	
+	});
+	
+	});
+
+
+	$('#ladyTramp').click(function(e){
+		e.preventDefault();
+		if ($('#ladyTramp').val() === "b"){
+			console.log('happening')
+			var lightbox = 
+			'<div id="lightbox">' +
+				'<p>Click to close</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<iframe width="560" height="315" src="https://www.youtube.com/embed/RzW4PH68KG4" frameborder="0" allowfullscreen></iframe>' +
+				'</div>' +	
+			'</div>';
+				
+			//insert lightbox HTML into page
+			$('body').prepend(lightbox);
+		}
+
+	//Click anywhere on the page to get rid of lightbox window
+	$('#lightbox').on('click', function() {
+		$('#lightbox').hide();	
+	});
+	
+	});
+
+
+	$('#goodfellas').click(function(e){
+		e.preventDefault();
+		if ($('#goodfellas').val() === "c"){
+			console.log('happening')
+			var lightbox = 
+			'<div id="lightbox">' +
+				'<p>Click to close</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<iframe width="560" height="315" src="https://www.youtube.com/embed/yztx8qfoNu0" frameborder="0" allowfullscreen></iframe>' +
+				'</div>' +	
+			'</div>';
+				
+			//insert lightbox HTML into page
+			$('body').prepend(lightbox);
+		}
+
+	//Click anywhere on the page to get rid of lightbox window
+	$('#lightbox').on('click', function() {
+		$('#lightbox').hide();	
+	});
+
+	});
+
+
+	$('#elf').click(function(e){
+		e.preventDefault();
+		if ($('#elf').val() === "d"){
+			console.log('happening')
+			var lightbox = 
+			'<div id="lightbox">' +
+				'<p>Click to close</p>' +
+				'<div id="content">' + //insert clicked link's href into img src
+					'<iframe width="560" height="315" src="https://www.youtube.com/embed/gUZSzHhXfc0" frameborder="0" allowfullscreen></iframe>' +
+				'</div>' +	
+			'</div>';
+				
+			//insert lightbox HTML into page
+			$('body').prepend(lightbox);
+		}
+
+	//Click anywhere on the page to get rid of lightbox window
+	$('#lightbox').on('click', function() {
+		$('#lightbox').hide();	
+	});
+
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
